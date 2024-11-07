2edb519e8f215b5d657403cca056c150773ef94f
Joey Kendall-Morwick <jbmorwick@gmail.com>
2024-11-06 21:59:58 -0600

    added exercise instructions

--- tictactoe/TicTacToe.java
+++ tictactoe/TicTacToe.java
@@ -1,5 +1,69 @@
-public class TicTacToe {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class TicTacToe {
+    private static char[][] board; 
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        board = new char[][]{
+            {'.','.','.'},
+            {'.','.','.'},
+            {'.','.','.'}};
+
+        char turn = 'X';
+        char winner = '-';
+        printBoard();
+
+        for(int placed=0; placed<9 && winner == '-'; placed++) {
+            System.out.printf("It is %c's turn%n", turn);
+            System.out.print("Enter row for move (1-3): ");
+            int r = in.nextInt() - 1;
+            System.out.print("Enter column for move (1-3): ");
+            int c = in.nextInt() - 1;
+
+             // challenge: prevent the turn from advancing if the user entered bad coordinates
+
+
+            board[r][c] = turn;
+            turn = turn == 'X' ? 'O': 'X';
+            printBoard();
+            winner = findWinner();
+        }
+        if(winner != '-') {
+            System.out.printf("Game Over: %c wins%n", winner);
+        } else {
+            System.out.printf("Game Over: no winner%n");
+        }
+    }
+
+    public static void printBoard() {
+        System.out.print("  1 2 3\n  -----\n");
+        for(int r=0; r<3; r++) {
+            System.out.print(r+1+" ");
+            for(int c=0; c<3; c++) {
+                System.out.print(board[r][c]);
+                if (c<2) System.out.print('|');
+            }
+            System.out.print("\n  -----\n");
+        }
+    }
+
+    public static char findWinner() {
+        for(int r=0; r<3; r++) {
+            // three-in-a-row horizontally
+            if(board[r][0] != '.' && board[r][0] == board[r][1] && board[r][1] == board[r][2]) {
+                return board[r][0];
+            }
+        }
+
+        // TODO: three-in-a-row vertically 
+
+        // diagonals
+        if(board[0][0] != '.' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return board[0][0];
+        }
+        // TODO: check other diagonal
+            
+        return '-';
     }
 }
--- tictactoe/TicTacToeTest.java
+++ tictactoe/TicTacToeTest.java
@@ -60,11 +60,47 @@ public class TicTacToeTest
         System.setErr(new PrintStream(new FileOutputStream(FileDescriptor.out)));
     }
 
-    @Test
-    public void checkpoint1Test()
-    {
-        mainIOTest("", ".|.|.\n-----\n.|.|.\n-----\n.|.|.");
-    }
 
+    
+    @Test
+    public void checkpoint7Test1()
+    {
+        mainIOTest( "2\n2\n1\n1\n3\n3\n1\n3\n3\n1\n1\n2\n", 
+                   "1 O|O|O\n  -----\n2 .|X|.\n  -----\n3 X|.|X\n  -----\n"+
+                   "Game Over: O wins");
+    }
+        
+    @Test
+    public void checkpoint7Test2()
+    {
+        mainIOTest("2\n2\n1\n1\n3\n3\n3\n1\n1\n2\n2\n1\n", 
+                   "1 O|X|.\n  -----\n2 O|X|.\n  -----\n3 O|.|X\n  -----\n"+
+                   "Game Over: O wins");
+    }
+        
+    @Test
+    public void checkpoint7Test3()
+    {
+        mainIOTest("1\n1\n1\n2\n1\n3\n2\n1\n2\n2\n2\n3\n3\n1\n", 
+                   "1 X|O|X\n  -----\n2 O|X|O\n  -----\n3 X|.|.\n  -----\n"+
+                   "Game Over: X wins");
+    }
+        
+    @Test
+    public void checkpoint7Test4()
+    {
+        mainIOTest("1\n2\n1\n1\n2\n2\n2\n1\n3\n2\n", 
+                   "1 O|X|.\n  -----\n2 O|X|.\n  -----\n3 .|X|.\n -----\n"+
+                   "Game Over: X wins");
+    }
+        
+    @Test
+    public void checkpoint7Test5()
+    {
+        mainIOTest("2\n2\n1\n1\n3\n1\n1\n3\n1\n2\n3\n2\n2\n1\n2\n3\n3\n3\n", 
+                   "1 O|X|O\n  -----\n2 X|X|O\n  -----\n3  X|O|X\n  -----\n"+
+                   "Game Over: no winner");
+    }    
+     
 }
 
